080c1b77ffd21d0591e1bebaa02cb5d172dea175
dandinoar <diego@andino.com.ar>
2018-10-28 12:23:35 -0400

    variant calling only snp

--- Tarea1/assignment_1.sh
+++ Tarea1/assignment_1.sh
@@ -12,7 +12,7 @@ mkdir -p c_variant_calling
 ########################
 fasta=$(ls ./raw_data| grep -P "$extension1$")
 echo "indexing '${fasta}' genome"
-#bowtie2-build raw_data/$fasta a_genome_index/genome_index > a_genome_index/    log.index
+bowtie2-build raw_data/$fasta a_genome_index/genome_index > a_genome_index/log.index
 echo "INDEX DONE. log file created: a_genome_index/log.index"
 
 
@@ -29,11 +29,11 @@ for i in $splited
     echo "log for '$i' file alignment" >> b_alignment/log.alignment
     basename=${i%"$extension2"}
     #make .sam
-#    bowtie2 -x a_genome_index/genome_index -U raw_data/$i -S   b_alignment/alignment_$basename.sam &>> b_alignment/log.alignment
+    bowtie2 -x a_genome_index/genome_index -U raw_data/$i -S   b_alignment/alignment_$basename.sam &>> b_alignment/log.alignment
     #Convert .sam to .bam
-#    samtools view -bS b_alignment/alignment_$basename.sam > b_alignment/alignment_$basename.bam
+    samtools view -bS b_alignment/alignment_$basename.sam > b_alignment/alignment_$basename.bam
     #Sort .bam file
-    #samtools sort b_alignment/alignment_$basename.bam b_alignment/alignment_$basename\_sorted
+    samtools sort b_alignment/alignment_$basename.bam b_alignment/alignment_$basename\_sorted
     done
 echo "ALIGNMENTS DONE. log file created: b_alignment/log.alignment"
 ##########################
@@ -46,11 +46,10 @@ splited=$(echo $bam | tr " " "\n")
 for i in $splited
     do
     echo "Variant Calling in '$i' file"
-    #echo "log for '$i' file call" >> c_variant_calling/log.variant_calling
+    echo "log for '$i' file call" >> c_variant_calling/log.variant_calling
     basename=${i%"$extension3"}
-#samtools mpileup -g -f raw_data/$fasta b_alignment/sorted_$basename.bam > c_variant_calling/variants_$basename.bcf
-    #Without indels (-I). Output .bcf (-g)
-    samtools mpileup -g -f raw_data/$fasta b_alignment/$basename.bam >  c_variant_calling/variants_$basename.bcf
+    #Without indels (-I). Output .bcf (-g). Automatic index .fai generation (-f)
+    samtools mpileup -Igf raw_data/$fasta b_alignment/$basename.bam >  c_variant_calling/variants_$basename.bcf
     bcftools view -vg c_variant_calling/variants_$basename.bcf > c_variant_calling/variants_$basename.vcf
  done
 echo "VARIANT CALLING DONE. log file created: c_variant_calling/log.variant_calling"
